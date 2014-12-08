ce4d83fa827917e05e7958c0c9c6a6611a8d5195
haibarahu <haibarahu@gmail.com>
2014-12-08 23:21:16 +0800

    add some common issues for arc converting

---
+++ JMHArcProjectForConverting/JMHArcProjectForConverting/JMHViewController.h
@@ -0,0 +1,24 @@
+//
+//  JMHViewController.h
+//  JMHArcProjectForConverting
+//
+//  Created by god on 12/8/14.
+//  Copyright (c) 2014 juanmao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol JMHViewControllerDelegate <NSObject>
+
+- (void)dosomething;
+
+@end
+
+@interface JMHViewController : UIViewController
+
+@property (nonatomic, retain) UIButton *button;
+@property (nonatomic, assign) id<JMHViewControllerDelegate> delegate;
+
+
+@end
+
---
+++ JMHArcProjectForConverting/JMHArcProjectForConverting/JMHViewController.m
@@ -0,0 +1,105 @@
+//
+//  ViewController.m
+//  JMHArcProjectForConverting
+//
+//  Created by god on 12/8/14.
+//  Copyright (c) 2014 juanmao. All rights reserved.
+//
+
+#import "JMHViewController.h"
+
+
+struct JMHStruct
+{
+    id x;
+    float y;
+};
+
+@interface JMHViewController()
+{
+    id<JMHViewControllerDelegate> assignDelegate;
+}
+
+@property (nonatomic, retain) UILabel *label;
+@property (nonatomic, assign) dispatch_queue_t myQueue;
+
+- (void)autoreleaseExample;
+- (void)retainReleaseExample;
+- (void)bridgeExample;
+- (void)coreFoundationExample;
+
+@end
+
+@implementation JMHViewController
+
+
+- (instancetype)init
+{
+    [super init];
+    self.label = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self retainReleaseExample];
+    [self autoreleaseExample];
+    [self bridgeExample];
+    [self coreFoundationExample];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)retainReleaseExample
+{
+    UIButton *button = [[UIButton alloc] init];
+    [[button retain] autorelease];
+    __unused NSUInteger buttonRetainCount = button.retainCount;
+    [button release];
+}
+
+
+- (void)autoreleaseExample
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    __unused UILabel *label = [[[UILabel alloc] init] autorelease];
+    
+    
+    for (int i =0; i < 10000; ++i) {
+        NSAutoreleasePool *innerPool = [[NSAutoreleasePool alloc] init];
+        __unused UIButton *button = [[[UIButton alloc] init] autorelease];
+        [innerPool release];
+    }
+    
+    [pool release];
+}
+
+- (void)bridgeExample
+{
+    NSString *exampleString = [NSString stringWithUTF8String:"juanmao"];
+    CFStringRef cfString = (CFStringRef)exampleString;
+    __unused NSString *string = (NSString *)cfString;
+}
+
+- (void)coreFoundationExample
+{
+    const void *num[3] = {"1", "2", "3"};
+    CFArrayRef cfArray = CFArrayCreate(kCFAllocatorDefault, num, 1, NULL);
+    CFRelease(cfArray);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc
+{
+    self.label = nil;
+    self.button = nil;
+    
+    [super dealloc];
+}
+
+@end
